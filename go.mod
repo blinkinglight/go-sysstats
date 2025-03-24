ede5db5a5885147f1c46818d453f155f48f54b4c
Mindaugas Žvirblis <mz@9g.lt>
2025-03-24 08:46:50 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,26 @@
+module github.com/blinkinglight/go-sysstats
+
+go 1.24.1
+
+require (
+	github.com/a-h/templ v0.3.856 // indirect
+	github.com/delaneyj/gostar v0.8.0 // indirect
+	github.com/ebitengine/purego v0.8.2 // indirect
+	github.com/go-chi/chi/v5 v5.2.1 // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/goccy/go-json v0.10.4 // indirect
+	github.com/igrmk/treemap/v2 v2.0.1 // indirect
+	github.com/inhies/go-bytesize v0.0.0-20220417184213-4913239db9cf // indirect
+	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
+	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
+	github.com/samber/lo v1.47.0 // indirect
+	github.com/shirou/gopsutil/v4 v4.25.2 // indirect
+	github.com/starfederation/datastar v1.0.0-beta.9 // indirect
+	github.com/tklauser/go-sysconf v0.3.12 // indirect
+	github.com/tklauser/numcpus v0.6.1 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/yusufpapurcu/wmi v1.2.4 // indirect
+	golang.org/x/exp v0.0.0-20250106191152-7588d65b2ba8 // indirect
+	golang.org/x/sys v0.31.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/inhies/go-bytesize"
+	"github.com/shirou/gopsutil/v4/cpu"
+	"github.com/shirou/gopsutil/v4/mem"
+	datastar "github.com/starfederation/datastar/sdk/go"
+)
+
+func main() {
+	r := chi.NewMux()
+
+	r.Get("/live/stats", func(w http.ResponseWriter, r *http.Request) {
+		sse := datastar.NewSSE(w, r)
+		ticker := time.NewTicker(1 * time.Second)
+		defer ticker.Stop()
+		for {
+			select {
+			case <-r.Context().Done():
+				return
+			case <-ticker.C:
+				v, _ := mem.VirtualMemory()
+				cp, _ := cpu.Times(false)
+				var stats Stats
+				stats.MemUsed = bytesize.New(float64(v.Used)).String()
+				stats.MemTotal = bytesize.New(float64(v.Total)).String()
+				stats.MemFree = bytesize.New(float64(v.Free)).String()
+				stats.MemPercent = v.UsedPercent
+
+				for _, c := range cp {
+					stats.CpuSystem += int(c.System)
+					stats.CpuUser += int(c.User)
+					stats.CpuIdle += int(c.Idle)
+				}
+
+				stats.CpuSystem /= len(cp)
+				stats.CpuUser /= len(cp)
+				stats.CpuIdle /= len(cp)
+
+				sse.MergeFragmentTempl(Stat(stats))
+			}
+		}
+	})
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		Main(Stats{}).Render(r.Context(), w)
+	})
+
+	log.Printf("Starting server on :8088")
+	http.ListenAndServe(":8088", r)
+}
